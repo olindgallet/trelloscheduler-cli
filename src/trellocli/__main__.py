81a3eafe3627c044c3d5ccd8b4e6f29b2ac9b2bc
Olin Gallet <olindgallet@olingallet.com>
2022-12-07 10:37:42 -0600

    Fixed error with tb instead of Trellobot

--- src/trellocli/__main__.py
+++ src/trellocli/__main__.py
@@ -13,28 +13,28 @@ if __name__ == '__main__':
         try:
             if sys.argv[1] == 'buildweek':
                 if Env.has_trello_keys():
-                    bot = tb.TrelloBot(Env.get_trello_api_key(), Env.get_trello_api_secret(), Env.get_trello_token())
+                    bot = tb(Env.get_trello_api_key(), Env.get_trello_api_secret(), Env.get_trello_token())
                     bot.build_weekly_board()
                     Terminal.display_buildweek_completion()
                 else:
                     raise EnvironmentVarsNotSetException()
             elif sys.argv[1] == 'startweek':
                 if Env.has_trello_keys():
-                    bot = tb.TrelloBot(Env.get_trello_api_key(), Env.get_trello_api_secret(), Env.get_trello_token())
+                    bot = tb(Env.get_trello_api_key(), Env.get_trello_api_secret(), Env.get_trello_token())
                     bot.create_weekly_lists()
                     Terminal.display_startweek_completion()
                 else:
                     raise EnvironmentVarsNotSetException() 
             elif sys.argv[1] == 'endweek':
                 if Env.has_trello_keys():
-                    bot = tb.TrelloBot(Env.get_trello_api_key(), Env.get_trello_api_secret(), Env.get_trello_token())
+                    bot = tb(Env.get_trello_api_key(), Env.get_trello_api_secret(), Env.get_trello_token())
                     bot.archive_all_lists()
                     Terminal.display_endweek_completion()
                 else:
                     raise EnvironmentVarsNotSetException()
             elif sys.argv[1] == 'loadweek' and len(sys.argv) == 3:
                 if Env.has_trello_keys():
-                    bot = tb.TrelloBot(Env.get_trello_api_key(), Env.get_trello_api_secret(), Env.get_trello_token())
+                    bot = tb(Env.get_trello_api_key(), Env.get_trello_api_secret(), Env.get_trello_token())
                     bot.upload_schedule(sys.argv[2])
                     Terminal.display_loadweek_completion()
                 else:
